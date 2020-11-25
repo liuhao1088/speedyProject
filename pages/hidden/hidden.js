33d8e221c214fc1a9dd1ad6cff03fc7394a02f07
liuhao1088 <1350569467@qq.com>
2020-11-25 14:09:38 +0800

    更新

--- pages/hidden/hidden.js
+++ pages/hidden/hidden.js
@@ -6,17 +6,18 @@ Page({
    */
   data: {
     item:{
-      url:'https://img11.360buyimg.com/ddimg/jfs/t1/131631/6/17228/111272/5fbdcaacE9f53afc6/c86722b146e8e518.jpg'
+      url:'https://img11.360buyimg.com/ddimg/jfs/t1/131631/6/17228/111272/5fbdcaacE9f53afc6/c86722b146e8e518.jpg',
+      urlList:[
+        'https://img14.360buyimg.com/ddimg/jfs/t1/132631/4/17306/59729/5fbdcabdE59bd0cfc/b407bd1e19922ea4.jpg',
+        'https://img11.360buyimg.com/ddimg/jfs/t1/131527/22/17022/57507/5fbdcac8E9a428ed9/f9d8eae03fad5836.jpg',
+        'https://img13.360buyimg.com/ddimg/jfs/t1/123079/35/19874/53483/5fbdcad3Eab459bbd/18bb6892b013f067.jpg',
+        'https://img11.360buyimg.com/ddimg/jfs/t1/130477/2/17236/71993/5fbdcadeE2d8e3870/e9a27a19ef9603b0.jpg',
+        'https://img14.360buyimg.com/ddimg/jfs/t1/142441/18/15735/81164/5fbdcae8E9aa59d78/281d9bd7dd7d8f9c.jpg',
+        'https://img12.360buyimg.com/ddimg/jfs/t1/143721/23/15731/74036/5fbdcafaE63b40341/8be78812efaf13a1.jpg',
+      
+      ]
     },
-    urlList:[
-      'https://img14.360buyimg.com/ddimg/jfs/t1/132631/4/17306/59729/5fbdcabdE59bd0cfc/b407bd1e19922ea4.jpg',
-      'https://img11.360buyimg.com/ddimg/jfs/t1/131527/22/17022/57507/5fbdcac8E9a428ed9/f9d8eae03fad5836.jpg',
-      'https://img13.360buyimg.com/ddimg/jfs/t1/123079/35/19874/53483/5fbdcad3Eab459bbd/18bb6892b013f067.jpg',
-      'https://img11.360buyimg.com/ddimg/jfs/t1/130477/2/17236/71993/5fbdcadeE2d8e3870/e9a27a19ef9603b0.jpg',
-      'https://img14.360buyimg.com/ddimg/jfs/t1/142441/18/15735/81164/5fbdcae8E9aa59d78/281d9bd7dd7d8f9c.jpg',
-      'https://img12.360buyimg.com/ddimg/jfs/t1/143721/23/15731/74036/5fbdcafaE63b40341/8be78812efaf13a1.jpg',
-    
-    ]
+   
   },
 
   /**
