d0c39ebef7ca6b3968465b36957fd08a4dad7f36
liuhao1088 <1350569467@qq.com>
2020-11-30 09:40:02 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,8 @@ Page({
       'https://img14.360buyimg.com/ddimg/jfs/t1/130213/8/17180/165679/5fbdba09Ec6de8f8f/93f75ee31016cc5b.jpg',
       'https://img13.360buyimg.com/ddimg/jfs/t1/139969/5/16038/290231/5fc06645E788f72e3/84c76231b63d581b.jpg',
       'https://img14.360buyimg.com/ddimg/jfs/t1/143409/39/15708/126373/5fbdc072Ee4b95bda/f554445ecfcd7075.jpg',
+      'https://img11.360buyimg.com/ddimg/jfs/t1/153419/32/7825/132329/5fc44d30E77052f32/0ddf7cbef34cc7c2.jpg',
+      'https://img13.360buyimg.com/ddimg/jfs/t1/130354/2/17891/124165/5fc44d52E2430dd51/99753bd2e106fa29.jpg'
     ],
     urlList:[
       'https://img13.360buyimg.com/ddimg/jfs/t1/136931/11/17480/245211/5fc1be48Ed4cf9af5/659ce151d856bbbd.png',
