f6fba74db8cf13a8ccf6269be0fe8497fafd1390
liuhao1088 <1350569467@qq.com>
2020-12-01 10:16:07 +0800

    +更新

--- pages/mSixa/mSixa.js
+++ pages/mSixa/mSixa.js
@@ -6,18 +6,15 @@ Page({
    */
   data: {
     item:{
-      url:'https://img11.360buyimg.com/ddimg/jfs/t1/148418/18/16064/509788/5fc1b5d4E57fe079e/8c76eab129938886.jpg',
+      url:'https://img13.360buyimg.com/ddimg/jfs/t1/136275/35/17283/404759/5fbf5cceE3a858100/595e49cd69aa9f56.jpg',
       videoUrl:'https://fnck-20200914-1303154931.cos.ap-guangzhou.myqcloud.com/%E8%A7%86%E9%A2%91/%E4%BF%9D%E9%80%9F%E6%8D%B7/%E4%BF%9D%E9%80%9F%E6%8D%B7M6A%E5%A4%96%E8%A7%82%20%E5%8E%BB%E6%B0%B4%E5%8D%B0.mp4?q-sign-algorithm=sha1&q-ak=AKIDHL7fPrFwXrtdX9SYLq75j5BxTeFbR3KK&q-sign-time=1606371567;10246285167&q-key-time=1606371567;10246285167&q-header-list=&q-url-param-list=&q-signature=7b880afab50300fa3f31649526668025fcb07e6f',
-      // detailList:[
-      //  'https://img10.360buyimg.com/ddimg/jfs/t1/132811/21/17432/478967/5fbf5ca5E782721b3/e268b28e5b282329.jpg',
-      //  'https://img14.360buyimg.com/ddimg/jfs/t1/133638/37/17414/320366/5fbf5cb2E58d371e0/764701470a43f4c2.jpg',
-      //  'https://img11.360buyimg.com/ddimg/jfs/t1/130509/27/17483/628463/5fbf5cc1E2991fac6/240635ed40cb1a8d.jpg',
-      //  'https://img13.360buyimg.com/ddimg/jfs/t1/136275/35/17283/404759/5fbf5cceE3a858100/595e49cd69aa9f56.jpg',
-      //  'https://img10.360buyimg.com/ddimg/jfs/t1/139820/33/15742/282445/5fbf5cd9Ee2d295eb/35da8810fcf07856.jpg',
-      //  'https://img13.360buyimg.com/ddimg/jfs/t1/142276/17/16048/570214/5fbf5ce7E3c548be9/5a4ba615813697e6.jpg',
-      //  'https://img10.360buyimg.com/ddimg/jfs/t1/145837/29/15902/294647/5fbf5cf5E75c5e37f/44923fcd339a32aa.jpg',
-      //  'https://img10.360buyimg.com/ddimg/jfs/t1/124712/1/20013/455744/5fbf5d04E016edd1f/df952c1e6ec03f2a.jpg',
-      //  ]
+      detailList:[
+       'https://img10.360buyimg.com/ddimg/jfs/t1/139820/33/15742/282445/5fbf5cd9Ee2d295eb/35da8810fcf07856.jpg',
+       'https://img10.360buyimg.com/ddimg/jfs/t1/124712/1/20013/455744/5fbf5d04E016edd1f/df952c1e6ec03f2a.jpg',
+       'https://img10.360buyimg.com/ddimg/jfs/t1/132811/21/17432/478967/5fbf5ca5E782721b3/e268b28e5b282329.jpg',
+       'https://img14.360buyimg.com/ddimg/jfs/t1/133638/37/17414/320366/5fbf5cb2E58d371e0/764701470a43f4c2.jpg',
+       'https://img10.360buyimg.com/ddimg/jfs/t1/145837/29/15902/294647/5fbf5cf5E75c5e37f/44923fcd339a32aa.jpg',
+       ]
     }
   },
 
